e77c159441a0efae82e8f899b7482330f8db1cbf
Graham Flaspoehler <grahamflas@gmail.com>
2025-07-18 12:06:34 +0200

    Update rails_heper to stop RSpec from truncating test results

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,6 +35,9 @@ begin
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
+
+RSpec::Support::ObjectFormatter.default_instance.max_formatted_output_length = nil
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
