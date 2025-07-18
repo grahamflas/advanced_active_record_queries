99ec029350eb9170042221be58edba2ada8d0a12
Graham Flaspoehler <grahamflas@gmail.com>
2025-07-18 12:05:47 +0200

    Update seeds to use find_or_create_by

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,52 +1,52 @@
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the bin/rails db:seed command (or find_or_create_by!d alongside the database with db:setup).
 #
 # Example:
 #
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
+#     MovieGenre.find_or_find_or_create_by!_by!(name: genre_name)
 #   end
-east = Region.create(name: "East")
-west = Region.create(name: "West")
-central_europe = Region.create(name: "Central Europe")
+east = Region.find_or_create_by!(name: "East")
+west = Region.find_or_create_by!(name: "West")
+central_europe = Region.find_or_create_by!(name: "Central Europe")
 
-boston = Location.create(name: "Boston", region: east)
-new_york = Location.create(name: "New York", region: east)
-denver = Location.create(name: "Denver", region: west)
-berlin = Location.create(name: "Berlin", region: central_europe)
+boston = Location.find_or_create_by!(name: "Boston", region: east)
+new_york = Location.find_or_create_by!(name: "New York", region: east)
+denver = Location.find_or_create_by!(name: "Denver", region: west)
+berlin = Location.find_or_create_by!(name: "Berlin", region: central_europe)
 
-developer = Role.create(
+developer = Role.find_or_create_by!(
   name: "Developer",
   billable: true,
 )
 
-manager = Role.create(
+manager = Role.find_or_create_by!(
   name: "Manager",
   billable: false,
 )
 
-Person.create(
+Person.find_or_create_by!(
   name: "Wendell",
   role: developer,
   location: boston,
 )
-Person.create(
+Person.find_or_create_by!(
   name: "Christie",
   role: developer,
   location: boston,
 )
-Person.create(
+Person.find_or_create_by!(
   name: "Sandy",
   role: developer,
   location: denver,
 )
-Person.create(
+Person.find_or_create_by!(
   name: "Eve",
   role: manager,
   location: new_york,
 )
-Person.create(
+Person.find_or_create_by!(
   name: "Graham",
   role: developer,
   location: berlin,
