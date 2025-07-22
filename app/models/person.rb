c66f6a5400d2197130a1e2002a03c4d56c99b69d
Graham Flaspoehler <grahamflas@gmail.com>
2025-07-22 14:37:48 +0200

    Set up Manager and Employees associations, naive implementation of #not_managed_by
    Set up self-joins for manager-employee associations.
    
    Start working on the scope `#not_managed_by`, with a naive implementation.
    
    We're correctly returning the employees who are not managed by the passed-in
    manager, but according to the specs, we should also be returning all managers,
    since managers have no manager and re therefore not managed by the passed in
    manager.
    
    The problem here is that we're using a `joins`, which does an `INNER JOIN` to
    `managers`, which will by definition exclude records where `manager_id` is
    `nil`.
    
    UP NEXT: refactor to use `LEFT JOIN`.

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,8 +2,22 @@ class Person < ApplicationRecord
   belongs_to :role
   belongs_to :location
 
+  belongs_to :manager,
+    class_name: "Person",
+    foreign_key: :manager_id,
+    optional: true
+  has_many :employees,
+    class_name: "Person",
+    foreign_key: :manager_id
+
   scope :billable, -> {
     joins(:role)
     merge(Role.billable)
+  }
+
+  scope :not_managed_by, -> (manager_id) {
+    joins(:manager).
+    where.
+    not(managers_people: { id: manager_id })
   }
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -25,5 +25,64 @@ RSpec.describe Person, type: :model do
         expect(result).not_to include(non_billable_person)
       end
     end
+
+    describe "not_managed_by" do
+      it "returns all people (managers and employees alike) whose manager is not the passed in manager_id" do
+        # managers
+        manager_1 = create(
+          :person,
+          name: "Manager 1",
+          manager_id: nil,
+        )
+        manager_2 = create(
+          :person,
+          name: "Manager 2",
+          manager_id: nil,
+        )
+        manager_3 = create(
+          :person,
+          name: "Manager 3",
+          manager_id: nil,
+        )
+
+        # employees
+        employee_a = create(
+          :person,
+          name: "Employee A",
+          manager: manager_1
+        )
+        employee_b = create(
+          :person,
+          name: "Employee B",
+          manager: manager_2
+        )
+        employee_c = create(
+          :person,
+          name: "Employee C",
+          manager: manager_2
+        )
+        employee_d = create(
+          :person,
+          name: "Employee D",
+          manager: manager_3
+        )
+
+        # Not managed by manager 1
+        results_1 = Person.not_managed_by(manager_1.id)
+
+        expect(
+          results_1.map(&:name)
+        ).to match_array(
+          [
+            manager_1,
+            manager_2,
+            manager_3,
+            employee_b,
+            employee_c,
+            employee_d,
+          ].map(&:name),
+        )
+      end
+    end
   end
 end
